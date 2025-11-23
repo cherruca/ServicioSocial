4ec624990bdc9d2bf476db9d06d6bf0cbdce1ae7
Ivan Nolasco <00402523@uca.edu.sv>
2025-11-23 00:45:47 -0600

    :test_tube: set up testing enviroment

---
+++ Backend/tests/student.test.js
@@ -0,0 +1,13 @@
+import { describe, test, it, expect } from 'vitest'
+
+describe('email', () => {
+    it('should return students info', () => {
+        // Arrange
+
+        // Act
+
+        // Assert
+
+    })
+
+})
